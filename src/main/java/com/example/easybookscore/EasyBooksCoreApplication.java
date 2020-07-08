666e1fddb072f967f5a04f7112fdb90222412859
dzmitrykalinin <dmitrij.kalinin.1994@mail.ru>
2020-07-08 21:54:24 +0300

    Create project structure with https://start.spring.io/

---
+++ src/main/java/com/example/easybookscore/EasyBooksCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.easybookscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBooksCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyBooksCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/easybookscore/EasyBooksCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easybookscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBooksCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
